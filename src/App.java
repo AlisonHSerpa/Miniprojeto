4a8cda5cd0ae5c410f839c0613cc6dc9642ce41f
Alison Serpa <alison.serpa@ufrpe.br>
2023-11-14 21:04:32 -0300

    Miniprojeto 4

--- src/App.java
+++ src/App.java
@@ -13,9 +13,8 @@ public class App {
         +"meu primeiro caso está quase no fim, persigo sorrateiramente meu suspeito, "
         +"estou armado so por segurança, ele acaba virando a esquerda na avenida Boulevard, "
         +"conheço esse beco, eh sem saida.'";
-        String op1 = "entrar escondido";
-        String op2 = "entrar junto";
-        Capitulo capitulo1 = new Capitulo(jogador, "perseguir", cap1, op1, op2, 0);
+        String[] escolhas = {"entrar junto", "entrar escondido"};
+        Capitulo capitulo1 = new Capitulo(jogador, "perseguir", cap1, escolhas, 0);
 
         //caítulo 2
         String cap2 = "'eu entro escondido, esperando alguns segundos encostado na parede, "
@@ -23,9 +22,8 @@ public class App {
         +"não consigo ouvir o que estão dizendo, apos agluns segundos,"
         +"vejo os dois sacarem suas armas um contra o outro! preciso agir rapido! "
         +"o que preciso fazer?'";
-        String op3 = "atirar no novo suspeito";
-        String op4 = "atirar no meu suspeito";
-        Capitulo capitulo2 = new Capitulo(jogador, "salve e condene", cap2, op3, op4, 1);
+        String[] escolhas2 = {"atirar no novo suspeito","atirar no meu suspeito"};
+        Capitulo capitulo2 = new Capitulo(jogador, "salve e condene", cap2, escolhas2, 1);
 
         //final 1
         String end1 = "'Este foi o ultimo registro escrito no caderno do detetive "
@@ -57,13 +55,13 @@ public class App {
         System.out.println("esse registro esta no nome de:");
         jogador.nome = sc.nextLine();
 
-        if (capitulo1.escolher() == 2){
+        if (capitulo1.escolher() == 0){
             suspeito1.atirar(jogador);
             suspeito2.atirar(jogador);
             final1.anunciar();
         }
         else {
-            if (capitulo2.escolher() == 1){
+            if (capitulo2.escolher() == 0){
                 jogador.atirar(suspeito2);
                 suspeito1.atirar(suspeito2);
                 final2.anunciar();
--- src/Capitulo.java
+++ src/Capitulo.java
@@ -3,19 +3,17 @@ public class Capitulo {
     Character protag;
     String nome;
     String texto;
-    String escolha01;
-    String escolha02;
+    String[] escolhas;
     int penalidade;
 
     Scanner sc = new Scanner(System.in);
 
     //construtor de capitulos
-    Capitulo(Character protag, String nome, String texto, String escolha01, String escolha02, int penalidade) {
+    Capitulo(Character protag, String nome, String texto, String[] escolhas, int penalidade) {
         this.protag = protag;
         this.nome = nome;
         this.texto = texto;
-        this.escolha01 = escolha01;
-        this.escolha02 = escolha02;
+        this.escolhas = escolhas;
         this.penalidade = penalidade;
     }
     //construtor de finais
@@ -30,23 +28,24 @@ public class Capitulo {
         this.protag.agir(this.penalidade);
         System.out.println("------" +this.nome+ "------");
         System.out.println(this.texto);
-        }
+    }
 
     public int escolher (){
-         while(true){
             anunciar ();
-            System.out.println(this.escolha01 + " / " + this.escolha02);
-            String escolhafinal = this.sc.nextLine();
-            
-            if (escolhafinal .equals(this.escolha01)){
-                return 1;
+            while(true){
+                for(int i=0 ; i<this.escolhas.length ;i++){
+                System.out.println(this.escolhas[i]);
+                }
+
+                String escolhafinal = this.sc.nextLine();
+
+                for (int i=0 ; i<this.escolhas.length ; i++){
+                    if (escolhafinal.equals(this.escolhas[i])){
+                        return i;
+                    }
+                }
+                System.out.println("voce digitou " +escolhafinal+ ", digite corretamente.");
+
+                }
             }
-            else if (escolhafinal .equals(this.escolha02)) {
-                return 2;
-            }
-            else {
-                System.out.println("voce digitou incorretamente, tente de novo:" + escolhafinal);
-            }
-        }
     }
-}
